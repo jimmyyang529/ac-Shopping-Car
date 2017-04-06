3ac548efb59825317fd0fc3a8f355ac3f2f701ae
jimmy yang <jimmyyang5296@gmail.com>
2017-04-06 13:03:29 +0800

    jimmy version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406042922) do
+ActiveRecord::Schema.define(version: 20170406050226) do
+
+  create_table "cats", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "goods", force: :cascade do |t|
     t.string   "product"
